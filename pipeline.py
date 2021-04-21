107b40400970537f85ac93b506045a0f61b38d8a
arkiver <arkiver@protonmail.com>
2021-04-21 16:56:11 +0200

    Version 20210421.02.

--- pipeline.py
+++ pipeline.py
@@ -52,7 +52,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20210421.01'
+VERSION = '20210421.02'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:52.0) Gecko/20100101 Firefox/59.0'
 TRACKER_ID = 'periscope'
 TRACKER_HOST = 'legacy-api.arpa.li'
