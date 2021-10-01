d7adbfb06caf6083c469570b73b8dc84573e0c98
arkiver <arkiver@protonmail.com>
2021-10-01 02:48:24 +0200

    Version 20211001.01. Use GNU Wget 1.20.3-at.20211001.01.

--- pipeline.py
+++ pipeline.py
@@ -39,7 +39,7 @@ if distutils.version.StrictVersion(seesaw.__version__) < distutils.version.Stric
 
 WGET_AT = find_executable(
     'Wget+AT',
-    ['GNU Wget 1.20.3-at.20210410.01'],
+    ['GNU Wget 1.20.3-at.20211001.01'],
     [
         './wget-at',
         '/home/warrior/data/wget-at'
@@ -55,7 +55,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20210421.03'
+VERSION = '20211001.01'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:52.0) Gecko/20100101 Firefox/59.0'
 TRACKER_ID = 'periscope'
 TRACKER_HOST = 'legacy-api.arpa.li'
